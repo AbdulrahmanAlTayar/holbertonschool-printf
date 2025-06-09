43dd6c25316a0397600955ecf574720c0da24e0e
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-06-09 18:39:40 +0300

    Refactor _printf: extract handle_specifier to meet Betty line limit

--- _printf.c
+++ _printf.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
 /**
+ * handle_specifier - Handles a format specifier
+ * @format: The format string
+ * @args: The argument list
+ * @i: Current index in format
+ * Return: Number of characters printed
+ */
+int handle_specifier(const char *format, va_list args, int *i)
+{
+	int count = 0;
+
+	switch (format[*i])
+	{
+	case 'c':
+		count += print_char(args);
+		break;
+	case 's':
+		count += print_string(args);
+		break;
+	case '%':
+		count += print_percent();
+		break;
+	default:
+		write(1, "%", 1);
+		write(1, &format[*i], 1);
+		count += 2;
+	}
+	return (count);
+}
+
+/**
  * _printf - Produces output according to a format
  * @format: Format string
  * Return: Number of characters printed
@@ -16,25 +46,12 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (format[i])
 	{
-		if (format[i] == '%' && format[i + 1])
+		if (format[i] == '%')
 		{
 			i++;
-			switch (format[i])
-			{
-				case 'c':
-					count += print_char(args);
-					break;
-				case 's':
-					count += print_string(args);
-					break;
-				case '%':
-					count += print_percent();
-					break;
-				default:
-					write(1, "%", 1);
-					write(1, &format[i], 1);
-					count += 2;
-			}
+			if (!format[i])
+				return (-1);
+			count += handle_specifier(format, args, &i);
 		}
 		else
 		{
